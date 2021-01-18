5d3571a86674418ed814cb5e1ddd4331c9d98cd0
mayhsin <71329989+mayhsin@users.noreply.github.com>
2021-01-19 03:04:02 +0800

    Add files via upload

---
+++ plan.js
@@ -0,0 +1,65 @@
+function Change1(){
+    But1.style.backgroundColor = "#FFD967";
+  }
+function Change2(){
+    But2.style.backgroundColor = "#f2aaaa";
+  }
+function Change3(){
+    But3.style.backgroundColor = "#e36387";
+  }
+  
+function Change4(){
+    But4.style.backgroundColor = "#FFD967";
+  }
+function Change5(){
+    But5.style.backgroundColor = "#f2aaaa";
+  }
+function Change6(){
+    But6.style.backgroundColor = "#e36387";
+  }
+function Change7(){
+    But7.style.backgroundColor = "#FFD967";
+  }
+function Change8(){
+    But8.style.backgroundColor = "#f2aaaa";
+  }
+function Change9(){
+    But9.style.backgroundColor = "#e36387";
+  }
+function Change10(){
+    But10.style.backgroundColor = "#FFD967";
+  }
+function Change11(){
+    But11.style.backgroundColor = "#f2aaaa";
+  }
+function Change12(){
+    But12.style.backgroundColor = "#e36387";
+  }
+function Change13(){
+    But13.style.backgroundColor = "#FFD967";
+  }
+function Change14(){
+    But14.style.backgroundColor = "#f2aaaa";
+  }
+function Change15(){
+    But15.style.backgroundColor = "#e36387";
+  }
+function Change16(){
+    But16.style.backgroundColor = "#FFD967";
+  }
+function Change17(){
+    But17.style.backgroundColor = "#f2aaaa";
+  }
+function Change18(){
+    But18.style.backgroundColor = "#e36387";
+  }
+function Change19(){
+    But19.style.backgroundColor = "#FFD967";
+  }
+function Change20(){
+    But20.style.backgroundColor = "#f2aaaa";
+  }
+function Change21(){
+    But21.style.backgroundColor = "#e36387";
+  }
+
